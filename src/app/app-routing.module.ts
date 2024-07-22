b6797ae38ab16e366c9a5bee1b32f1992ae0340f
Musaib Gul <dev.renosoftwares@gmail.com>
2024-07-22 11:36:59 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',redirectTo:'web',pathMatch:"full"},
+  { path: 'web', loadChildren: () => import('./main/website/website.module').then(m => m.WebsiteModule) },
+  { path: 'super', loadChildren: () => import('./main/super-admin/super-admin.module').then(m => m.SuperAdminModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
